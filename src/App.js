19675539b2c712b28f7537adc110506bd55bc1c2
konohazukux <konohazukux@gmail.com>
2022-05-28 13:46:57 +0900

    fix

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import { useState } from "react";
 import { ChildArea } from "./components/ChildArea";
 import "./styles.css";
 
 export default function App() {
+  // open
+  const [text, setText] = useState();
+  const [open, setOpen] = useState(false);
+
+  const onClickText = (e) => setText(e.target.value);
+  const onClickOpen = () => setOpen(!open);
+
   return (
     <div className="App">
-      <input type="text" />
+      <input type="text" value={text} onClick={onClickText} />
+
       <br />
       <br />
-      <button>表示</button>
-      <ChildArea />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} />
     </div>
   );
 }
--- src/components/ChildArea.jsx
+++ src/components/ChildArea.jsx
@@ -1,5 +1,6 @@
 import { React } from "react";
 
-export const ChildArea = () => {
-  return <div>子コンポーネント</div>;
+export const ChildArea = (props) => {
+  const { open } = props;
+  return <>{open ? <div>子コンポーネント</div> : null}</>;
 };
