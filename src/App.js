1ebadae74bc0b126a68174cafa42cc4ea74a8de8
konohazukux <konohazukux@gmail.com>
2022-05-28 15:44:45 +0900

    13 css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import { useState } from "react";
-import { ChildArea } from "./components/ChildArea";
+import { InlineStyle } from "./components/InlineStyle";
 import "./styles.css";
 
 export default function App() {
-  // open
-  const [text, setText] = useState();
-  const [open, setOpen] = useState(false);
-
-  const onClickText = (e) => setText(e.target.value);
-  const onClickOpen = () => setOpen(!open);
-
   return (
     <div className="App">
-      <input type="text" value={text} onClick={onClickText} />
-
-      <br />
-      <br />
-      <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <InlineStyle />
     </div>
   );
 }
---
+++ src/components/InlineStyle.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+// containerStyle
+const containerStyle = {
+  border: "1px solid #392eff",
+  borderRadius: "20px",
+  padding: "8px",
+  margin: "8px",
+  display: "flex",
+  justifyContent: "space-around",
+  alignItems: "center"
+};
+const titleStyle = {
+  margin: 0,
+  color: "#3d84a8"
+};
+const buttonStyle = {
+  backgroundColor: "#abedd8",
+  border: "none",
+  padding: "8px",
+  borderRadius: "8px"
+};
+export const InlineStyle = (props) => {
+  return (
+    <div style={containerStyle}>
+      <p style={titleStyle}> - Inline Styles -</p>
+      <button style={buttonStyle}>FIGHT!!</button>
+    </div>
+  );
+};
