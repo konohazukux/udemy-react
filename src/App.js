fe7421cfaa093412cbc6f2fe45fb2ad43102b991
konohazukux <konohazukux@gmail.com>
2022-05-28 16:00:22 +0900

    14 scss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import { InlineStyle } from "./components/InlineStyle";
+import { CssModules } from "./components/CssModules";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <InlineStyle />
+      <CssModules />
     </div>
   );
 }
